69c9bf78305509c7e7bfe11469d686a64ee9241d
Doncho Balamjiev <doncho_cska@mail.bg>
2020-05-11 23:33:54 +0300

    Personal Project Management Tool

---
+++ src/main/java/com/doncho/ppmtool/services/ProjectService.java
@@ -0,0 +1,20 @@
+package com.doncho.ppmtool.services;
+
+import com.doncho.ppmtool.domain.Project;
+import com.doncho.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService
+{
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project)
+    {
+        //Logic
+        return  projectRepository.save(project);
+    }
+
+}
---
+++ src/main/java/com/doncho/ppmtool/web/ProjectController.java
@@ -0,0 +1,34 @@
+package com.doncho.ppmtool.web;
+
+import com.doncho.ppmtool.domain.Project;
+import com.doncho.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController
+{
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping("")
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result)
+    {
+        if(result.hasErrors())
+        {
+            return new ResponseEntity<String>("Invalid object:[" + result.getAllErrors()+"]", HttpStatus.BAD_REQUEST);
+        }
+
+        Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+}
