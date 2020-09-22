49510ef24f2d4854ce59205c82ae0bac02bf90dd
Doncho Balamjiev <doncho_cska@mail.bg>
2020-09-22 10:47:39 +0300

    Personal Project Management Tool

--- src/main/java/com/doncho/ppmtool/sequrity/SecurityConstants.java
+++ src/main/java/com/doncho/ppmtool/sequrity/SecurityConstants.java
@@ -7,6 +7,6 @@ public class SecurityConstants
     public static final String SECRET = "SecretKeyToGenerateJwTs";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 300_000; // 30_000= 30 sec
+    public static final long EXPIRATION_TIME = 3000_000; // 30_000= 30 sec
 
 }
